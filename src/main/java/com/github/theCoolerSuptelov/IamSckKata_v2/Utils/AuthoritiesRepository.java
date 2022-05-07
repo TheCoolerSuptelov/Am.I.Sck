1a27d69b493e905054106f7b24dc685ac5677d01
Kirill Suptelov <kirill.suptelov@gmail.com>
2022-05-07 23:07:54 +0700

    new file:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Utils/AuthoritiesRepository.java 	new file:   src/main/resources/db/migration/V2__DataMigration.sql     modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Clients/ClientService.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Clients/Clients.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Clients/ClientsController.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Clients/ClientsDto.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Clients/ClientsRepository.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Nurses/Nurse.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Utils/Actor.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Utils/ActorService.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Utils/AuthoritiesRepository.java 	modified:   src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Utils/Configs/ImSckSecutiryConfig.java 	modified:   src/main/resources/application.properties 	modified:   src/main/resources/db/migration/V2__DataMigration.sql

---
+++ src/main/java/com/github/theCoolerSuptelov/IamSckKata_v2/Utils/AuthoritiesRepository.java
@@ -0,0 +1,4 @@
+package com.github.theCoolerSuptelov.IamSckKata_v2.Utils;
+
+public interface AuthoritiesRepository extends org.springframework.data.jpa.repository.JpaRepository<com.github.theCoolerSuptelov.IamSckKata_v2.Utils.Authorities, java.util.UUID> {
+}
